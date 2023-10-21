e7a635afcc7c0dca877de7e875e2787de013e789
Stephanie <syng828@gmail.com>
2023-10-21 15:20:53 -0700

    new xml code, with jasleen help

--- data-parsing.py
+++ data-parsing.py
@@ -1,6 +1,6 @@
 import json
 import csv
-
+import xml.etree.ElementTree as ET
 
 def read_tab_file(file_name): 
     data = []
@@ -25,6 +25,21 @@ def save_csv(data, output_file):
         writer.writeheader()
         writer.writerows(data)
 
+def save_xml(input_file, output_file):
+    # Define the XML root element
+    root = ET.Element("Data")
+
+    # Open the text file for reading
+    with open(input_file, 'r', encoding='iso-8859-1') as file:
+        for line in file:
+            # Create a new XML element for each line
+            element = ET.SubElement(root, 'Line')
+            element.text = line.strip()
+
+    # Create an ElementTree and write it to an XML file
+    tree = ET.ElementTree(root)
+    tree.write(output_file)
+
 if __name__ == '__main__': 
     file_name = input("Please enter the file name: "); 
     conversion = input("Enter -c to convert to CSV, -j to convert to JSON and -x to convert to XML: ")
@@ -37,5 +52,9 @@ if __name__ == '__main__':
         csv_output_file = file_name.split('.')[0] + '.csv'
         save_csv(data, csv_output_file)
         print("Converted to CSV")
+    elif (conversion == '-x'):
+        xml_output_file = file_name.split('.')[0] + '.xml'
+        save_xml(file_name, xml_output_file)
+        print("Converted to XML")
 
     
